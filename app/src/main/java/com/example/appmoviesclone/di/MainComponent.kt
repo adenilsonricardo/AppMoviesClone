d0b113e8a3abbd46748dab8b85a1b2601e290f7b
adenilson <adenilson.bernardes@bol.com.br>
2021-08-09 10:49:37 -0300

    dagger implementation

---
+++ app/src/main/java/com/example/appmoviesclone/di/MainComponent.kt
@@ -0,0 +1,16 @@
+package com.example.appmoviesclone.di
+
+import android.app.Activity
+import com.example.appmoviesclone.MainActivity
+import com.example.appmoviesclone.ui.HomeFragment
+import dagger.Subcomponent
+
+@Subcomponent(modules = [])
+interface MainComponent {
+    interface Factory{
+        fun create(): MainComponent
+    }
+
+    fun inject(activity: MainActivity)
+    fun inject(fragment: HomeFragment)
+}
