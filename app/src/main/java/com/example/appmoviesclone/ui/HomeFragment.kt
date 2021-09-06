02006396175f07dfc49d30847a2dd764e600ab31
adenilson <adenilson.bernardes@bol.com.br>
2021-09-06 10:54:50 -0300

    Creantin Databiding and View Binding part01

--- app/src/main/java/com/example/appmoviesclone/ui/HomeFragment.kt
+++ app/src/main/java/com/example/appmoviesclone/ui/HomeFragment.kt
@@ -1,15 +1,19 @@
 package com.example.appmoviesclone.ui
 
 import android.content.Context
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.appmoviesclone.MainActivity
 import com.example.appmoviesclone.R
+import com.example.appmoviesclone.databinding.HomeFragmentBinding
+import com.example.appmoviesclone.viewmodel.HomeViewModel
 import javax.inject.Inject
 
 class HomeFragment : Fragment() {
@@ -20,12 +24,28 @@ class HomeFragment : Fragment() {
         super.onAttach(context)
         (requireActivity() as MainActivity).mainComponent.inject(this)
     }
+    private lateinit var binding: HomeFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
-        viewModel.getTrending()
-        return view
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.home_fragment, container, false
+        )
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        attachObsevers()
+        return binding.root
+    }
+    private fun attachObservers() {
+        listsOfMoviesObserver()
+    }
+    private fun listsOfMoviesObserver() {
+        viewModel.listsOfMovies?.observe(viewLifecycleOwner, Observer { lists ->
+            lists?.let {
+
+            }
+        })
     }
 }
